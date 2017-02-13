ca12a70c6797fbda5c2b40a6ae24115509bc39f1
Shikha15698 <Shikha.arun6@gmail.com>
2017-02-13 14:42:35 +0530

    Update main.c

---
+++ week1/main.c
@@ -0,0 +1,16 @@
+/write your code in this window. 
+ #include<stdio.h>
+ int main()
+ {
+   int a=15, b=23;
+   printf("the sum is= %d \n", process(a,b));
+   return 0;    
+ }
+ 
+ 
+   
+  int process(int a, int b)
+  { 
+      return( a+b);
+  }
+  
